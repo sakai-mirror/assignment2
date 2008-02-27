afcaa4a56db10fe1db1fc2f14cac52102897ab97
stuart.freeman@et.gatech.edu <stuart.freeman@et.gatech.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-02-27 15:06:09 +0000

    radio buttons
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@46030 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/producers/UploadAllProducer.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/producers/UploadAllProducer.java
@@ -49,7 +49,9 @@ public class UploadAllProducer implements ViewComponentProducer, ViewParamsRepor
 		// Render release options radios
 		String[] release_selectValues = new String[] {"release", "norelease"};
 		String[] release_selectKeys = new String[] {"assignment2.uploadall.release", "assignment2.uploadall.norelease"};
-		UISelect.make(upload_form, "releaseSelect", release_selectValues, release_selectKeys, "", true).setMessageKeys();
+		UISelect releaseSelect = UISelect.make(upload_form, "releaseSelect", release_selectValues, release_selectKeys, "", true).setMessageKeys();
+		UISelectChoice.make(upload_form, "release", releaseSelect.getFullID(), 0);
+		UISelectChoice.make(upload_form, "norelease", releaseSelect.getFullID(), 1);
 		
 		// Render buttons
 		UICommand.make(upload_form, "uploadButton", UIMessage.make("assignment2.uploadall.upload"), "");
