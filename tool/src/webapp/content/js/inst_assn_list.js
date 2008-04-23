7dd5d93f4726654890f0c2fcf8a8cc27558d1a75
stuart.freeman@et.gatech.edu <stuart.freeman@et.gatech.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-04-23 19:17:54 +0000

    Merge branch 'tablesort'
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/iteration_5@48268 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/inst_assn_list.js
+++ tool/src/webapp/content/js/inst_assn_list.js
@@ -6,4 +6,9 @@ $(document).ready(function()
 	// add the toggle events to the twisties
 	ListCommon.addToggle('#postedTwisty', '#postedList', true);
 	ListCommon.addToggle('#draftsTwisty', '#draftsList', true);
+
+	// Make the tables sortable
+	$("#draftAssns").tablesorter({headers: { 0: { sorter: false}}});
+	$("#postedAssns").tablesorter();
+
 });
