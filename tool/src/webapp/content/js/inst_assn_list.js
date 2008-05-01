898c14b0e5331ded95849ee3b7aa1a7b48248b9d
stuart.freeman@et.gatech.edu <stuart.freeman@et.gatech.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-05-01 18:10:14 +0000

    Merge branch 'ejs'
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/iteration_5@48562 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/inst_assn_list.js
+++ tool/src/webapp/content/js/inst_assn_list.js
@@ -1,5 +1,20 @@
 $(document).ready(function()
 {
+	// Populate the tables
+	var testdata = {
+		"drafts" : [
+			{ "id" : "3", "title" : "Assignment 3", "sections" : "A1", "openDate" : "05/21/2008", "dueDate" : "06/21/2008" },
+			{ "id" : "4", "title" : "Assignment 4", "sections" : "A2", "openDate" : "05/22/2008", "dueDate" : "06/22/2008" }
+		],
+		"posted" : [
+			{ "id" : "1", "title" : "Assignment 1", "sections" : "A3", "openDate" : "04/21/2008",  "dueDate" : "05/21/2008" },
+			{ "id" : "2", "title" : "Assignment 2", "sections" : "A4", "openDate" : "04/22/2008", "dueDate" : "05/22/2008" }
+		]
+	};
+
+	new EJS({element: 'draft_template'}).update('draft_out', testdata);
+	new EJS({element: 'posted_template'}).update('posted_out', testdata);
+
 	// add a hover effect to each row of data
 	ListCommon.addHover('.dataRow', 'dataRowOn');
 
--- tool/src/webapp/content/js/inst_sub_list.js
+++ tool/src/webapp/content/js/inst_sub_list.js
@@ -1,20 +1,50 @@
 $(document).ready(function()
 {
+	// Populate the tables
+	var testdata = {
+		"assignments" : [
+			{
+				"id" : "3", "title" : "Assignment 3", "type" : "electronic",
+				"submitted" : [
+					{ "name" : "Carl Hall", "submittedOn" : "04/20/2008", "dueDate" : "04/21/2008", "sections" : "A1" },
+					{ "name" : "Stuart Freeman", "submittedOn" : "04/19/2008", "dueDate" : "04/21/2008", "sections" : "A1" }
+				],
+				"completed" : [
+					{ "name" : "Tom Jones", "returnedOn" : "04/22/2008", "returnedBy" : "Clay Fenlason", "feedback" : "Well done" },
+					{ "name" : "Clay Fenlason", "returnedOn" : "04/25/2008", "returnedBy" : "Clay Fenlason", "feedback" : "Well done" }
+				]
+			},
+			{
+				"id" : "4", "title" : "Assignment 4", "type" : "non-electronic",
+				"submitted" : [
+					{"name" : "Carl Hall", "sections" : "A1", "feedback" : "good job" },
+					{"name" : "Stuart Freeman", "sections" : "A1", "feedback" : "good job" }
+				],
+				"completed" : [
+					{"name" : "Tom Jones", "returnedOn" : "04/22/2008", "returnedBy" : "Clay Fenlason", "feedback" : "Well done" },
+					{"name" : "Clay Fenlason", "returnedOn" : "04/25/2008", "returnedBy" : "Clay Fenlason", "feedback" : "Well done" }
+				]
+			}
+		]
+	};
+
+	new EJS({element: 'template'}).update('output', testdata);
+
 	// add a hover effect to each row of data
 	ListCommon.addHover('.dataRow', 'dataRowOn');
 
 	// add the toggle events to the twisties
-	ListCommon.addToggle('#submitted1Twisty', '#submitted1List', true);
-	ListCommon.addToggle('#completed1Twisty', '#completed1List');
-
-	ListCommon.addToggle('#submitted2Twisty', '#submitted2List', true);
-	ListCommon.addToggle('#completed2Twisty', '#completed2List');
+	var toggles = $('.twisty');
+	for (var i=0; i<toggles.length; i++) {
+		var show = (toggles[i].id.substring(0,9) == 'submitted')
+		ListCommon.addToggle(toggles[i].id, toggles[i].id.replace('Twisty', 'List'), show);
+	};
 
 	$('.dataRow').click(function()
 	{
 		location.href='submissionview.html';
 	});
-	
+
 	// accordion by assignment
 	jQuery('#assignmentList').accordion({
 		header: 'div.subheader',
@@ -24,9 +54,6 @@ $(document).ready(function()
 	});
 
 	//Make tables sortable
-	$("#submitted1Assns").tablesorter();
-	$("#completed1Assns").tablesorter();
-	$("#submitted2Assns").tablesorter();
-	$("#completed2Assns").tablesorter();
+	$(".tablesorter").tablesorter();
 
 });
