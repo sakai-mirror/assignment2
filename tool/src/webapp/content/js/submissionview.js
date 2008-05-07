0a7cde91fb9e8d17859fc92da45b9f950acb292d
stuart.freeman@et.gatech.edu <stuart.freeman@et.gatech.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-05-07 20:05:59 +0000

    added sample json
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/iteration_5@48746 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/submissionview.js
+++ tool/src/webapp/content/js/submissionview.js
@@ -1,4 +1,24 @@
 var InstSubmView = {
+	var testdata = {
+		"id" : "3", "title" : "Assignment 3", "type" : "electronic",
+		"assignment" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		"drafts" : [
+			{ "id" : "first draft", "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			}
+		],
+		"submission" : "Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		"attachments" : [
+			{ "id" : "Untitled.doc", "url" : "http://example.com/Untitled.doc" }
+		],
+		"resources" : [
+			{ "id" : "MyFeed", "url" : "http://lipsum.com/feed/lipsum.com/feed/html" }
+		],
+		"feedback" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+		"history" : [
+			{ "revision" : "1", "date" : "1/1/1970", "url" : "http://example.com/rev/1" }
+		]
+	};
+
 	addToggle : function(id, alreadyClosed)
 	{
 		var contentId = id + '-content';
