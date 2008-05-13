d496dd51de25dcee493be50e18d760e79c8a09c1
clay.fenlason@et.gatech.edu <clay.fenlason@et.gatech.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-05-13 04:17:15 +0000

    Switched icons
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/iteration_5@48930 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/list_common.js
+++ tool/src/webapp/content/js/list_common.js
@@ -5,12 +5,12 @@ var ListCommon = function()
 		{
 			var onClose = function()
 			{
-				$(twisty + ' > img:first').attr('src', '../images/bullet_toggle_plus.png');
+				$(twisty + ' > img:first').attr('src', '../images/widget1-collapse-arrow.png');
 				$(area).slideUp('fast');
 			};
 			var onOpen = function()
 			{
-				$(twisty + ' > img:first').attr('src', '../images/bullet_toggle_minus.png');
+				$(twisty + ' > img:first').attr('src', '../images/widget1-arrow.png');
 				$(area).slideDown('fast');
 			};
 			if (normallyOpen)
