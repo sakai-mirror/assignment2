1ecbde5cbeeecea3dae5350b677103fd14f9191d
carl.hall@et.gatech.edu <carl.hall@et.gatech.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-05-16 17:36:11 +0000

    Wired up assignment creation workflow page 1.
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/iteration_5@49097 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/newassignment1.js
+++ tool/src/webapp/content/js/newassignment1.js
@@ -4,4 +4,16 @@ jQuery(document).ready(function()
 	var context = qs.get('context');
 
 	jQuery('#context').val(context);
+
+	var id = qs.get('id');
+	if (id)
+	{
+		jQuery('#id').val(id);
+		var url = '/sakai-assignment2-tool/sdata/newAsnn1?id=' + id;
+		jQuery.getJSON(url, function(data)
+		{
+			jQuery('#title').val(data['title']);
+			jQuery('#instructions').val(data['instructions']);
+		});
+	}
 });
