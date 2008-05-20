13a86d6f0c0287853b2d1a09286c534e4c03577e
carl.hall@et.gatech.edu <carl.hall@et.gatech.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-05-20 13:30:12 +0000

    Minor fix to step 3 js.
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/iteration_5@49227 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/newassignment3.js
+++ tool/src/webapp/content/js/newassignment3.js
@@ -16,39 +16,9 @@ jQuery(document).ready(function()
 		var url = '/sakai-assignment2-tool/sdata/newAsnn3?id=' + id;
 		jQuery.getJSON(url, function(data)
 		{
-			switch (data['resubmissions'])
-			{
-				case -1:
-					jQuery('#perpetual').attr('checked', 'checked');
-					break;
-				case 1:
-					jQuery('#oneSubmit').attr('checked', 'checked');
-					break;
-				default:
-					jQuery('#resubmitLimit').attr('checked', 'checked');
-					break;
-			}
-			switch (data['notifications'])
-			{
-				case 0:
-					jQuery('#dontSend').attr('checked', 'checked');
-					break;
-				case 1:
-					jQuery('#sendEach').attr('checked', 'checked');
-					break;
-				case 2:
-					jQuery('#sendDigest').attr('checked', 'checked');
-					break;
-			}
-			switch (data['honorCode'])
-			{
-				case true:
-					jQuery('#honor').attr('checked', 'checked');
-					break;
-				case false:
-					jQuery('#noHonor').attr('checked', 'checked');
-					break;
-			}
+			jQuery("input[name='resubmissions'][value='" + data['resubmissions'] + "']").attr('checked', 'checked');
+			jQuery("input[name='notifications'][value='" + data['notifications'] + "']").attr('checked', 'checked');
+			jQuery("input[name='honorcode'][value='" + data['honorCode'] + "']").attr('checked', 'checked');
 		});
 	}
 });
