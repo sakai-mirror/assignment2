0a1506a00dd5f5b3fbaee31ebc8555bcec564b62
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-09-08 19:23:44 +0000

    NOJIRA Minor code reformatting
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@52862 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/params/FilePickerHelperViewParams.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/params/FilePickerHelperViewParams.java
@@ -23,22 +23,22 @@ package org.sakaiproject.assignment2.tool.params;
 
 public class FilePickerHelperViewParams extends ThickboxHelperViewParams {
 
-	public String otpkey;
-	
-	public FilePickerHelperViewParams() {}
+    public String otpkey;
+
+    public FilePickerHelperViewParams() {}
 
     public FilePickerHelperViewParams(String viewId, String otpkey){
-    		super(viewId);
-	        this.otpkey = otpkey;
+        super(viewId);
+        this.otpkey = otpkey;
     }
-    
+
     public FilePickerHelperViewParams(String viewId, Boolean KeepThis, Boolean TB_iframe, int height, int width, String otpkey){
-    	super(viewId, KeepThis, TB_iframe, height, width);
-    	this.otpkey = otpkey;
+        super(viewId, KeepThis, TB_iframe, height, width);
+        this.otpkey = otpkey;
     }
-    
+
     public String getParseSpec() {
-		// include a comma delimited list of the public properties in this class
-		return super.getParseSpec() + ",otpkey";
-	}
+        // include a comma delimited list of the public properties in this class
+        return super.getParseSpec() + ",otpkey";
+    }
 }
