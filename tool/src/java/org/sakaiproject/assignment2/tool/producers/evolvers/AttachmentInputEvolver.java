b2bb7dd63f371d4a52149482d186bf7d5a1383f5
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-09-08 21:06:51 +0000

    ASSN-201 Fixed evolver for 0.7.3 behavior
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@52872 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/producers/evolvers/AttachmentInputEvolver.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/producers/evolvers/AttachmentInputEvolver.java
@@ -20,99 +20,94 @@ import org.sakaiproject.assignment2.logic.ExternalLogic;
 
 
 public class AttachmentInputEvolver {
-	
-	public static final String COMPONENT_ID = "attachment-list:";
-	public static final String CORE_ID = "attachment-list-core:";
-	
-	private ContentHostingService contentHostingService;
-	public void setContentHostingService(ContentHostingService contentHostingService) {
-		this.contentHostingService = contentHostingService;
-	}
-	
-	private ExternalLogic externalLogic;
-	public void setExternalLogic(ExternalLogic externalLogic) {
-		this.externalLogic = externalLogic;
-	}
-	
-	private MessageLocator messageLocator;
-	public void setMessageLocator(MessageLocator messageLocator) {
-		this.messageLocator = messageLocator;
-	}
-	
-	private BeanGetter rbg;
-	public void setRequestBeanGetter(BeanGetter rbg) {
-	    this.rbg = rbg;
-	}
-	public UIJointContainer evolveAttachment(UIInputMany toevolve) {
-		//TODO FIXME SWG
-		// The parent seems to be getting nulled after the toevolve.parent.remove
-		// call.
-		UIContainer oldparent = toevolve.parent;
-		toevolve.parent.remove(toevolve);
-		//UIJointContainer togo = new UIJointContainer(toevolve.parent, toevolve.ID, COMPONENT_ID);
-		UIJointContainer togo = new UIJointContainer(oldparent, toevolve.ID, COMPONENT_ID);
-		
-		toevolve.ID = "attachments-input";
-		togo.addComponent(toevolve);
-		
-		String[] value = toevolve.getValue();
-		// Note that a bound value is NEVER null, an unset value is detected via
-	    // this standard call
-	    if (UITypes.isPlaceholder(value)) {
-	      value = (String[]) rbg.getBean(toevolve.valuebinding.value);
-	      // May as well save on later fixups
-	      toevolve.setValue(value);
-	    }
 
-		UIBranchContainer core = UIBranchContainer.make(togo, CORE_ID);
-		int limit = Math.max(0, value.length);
-		for (int i=0; i < limit; ++i) {
-			UIBranchContainer row = UIBranchContainer.make(core, "attachment-list-row:", Integer.toString(i));
-			String thisvalue = i < value.length ? value[i] : "";
-			try {
-				//TODO - but all contentHosting calls in an external Logic
-				ContentResource cr = contentHostingService.getResource(thisvalue);
-				UILink.make(row, "attachment_image", externalLogic.getContentTypeImagePath(cr));
-				UILink.make(row, "attachment_link", cr.getProperties().getProperty(cr.getProperties().getNamePropDisplayName()),
-	    				cr.getUrl());
-				UIOutput.make(row, "attachment_item", thisvalue);
-				UIBasicListMember.makeBasic(row, "attachment_item", toevolve.getFullID(), i);
-				String file_size = externalLogic.getReadableFileSize(cr.getContentLength());
-	    		UIOutput.make(row, "attachment_size", file_size);
-	    		//Add remove link
-    			UIVerbatim.make(row, "attachment_remove", 
-    					"<a href=\"#\" " +
-    					"onclick=\"" +
-    					"jQuery(this).parent('span').parent('li').remove();updateDisplayNoAttachments();" +
-    					"\">" +
-    					messageLocator.getMessage("assignment2.remove") +
-    					"</a>");
-			} catch (Exception e) {
-				//do nothing
-			}
-		}
-		
-		if (limit == 0) {
-			//output "demo" row, with styleClass of skip
-			UIBranchContainer row = UIBranchContainer.make(core, "attachment-list-row:", Integer.toString(0));
-			UILink.make(row, "attachment_image", "image.jpg");
-			UILink.make(row, "attachment_link", "demo", "demo.html");
-			//UIOutput.make(row, "attachment_item", "demo");
-			UIBasicListMember.makeBasic(row, "attachment_item", toevolve.getFullID(), 0);
-			UIOutput.make(row, "attachment_size", "demo");
-			//Add remove link
-			UIVerbatim.make(row, "attachment_remove", 
-					"<a href=\"#\" " +
-					"onclick=\"" +
-					"jQuery(this).parent('span').parent('li').remove();updateDisplayNoAttachments();" +
-					"\">" +
-					messageLocator.getMessage("assignment2.remove") +
-					"</a>");
-    		row.decorators = new DecoratorList(new UIStyleDecorator("skip"));
-		}
-		
-		return togo;
-	}
-	
-	
+    public static final String COMPONENT_ID = "attachment-list:";
+    public static final String CORE_ID = "attachment-list-core:";
+
+    private ContentHostingService contentHostingService;
+    public void setContentHostingService(ContentHostingService contentHostingService) {
+        this.contentHostingService = contentHostingService;
+    }
+
+    private ExternalLogic externalLogic;
+    public void setExternalLogic(ExternalLogic externalLogic) {
+        this.externalLogic = externalLogic;
+    }
+
+    private MessageLocator messageLocator;
+    public void setMessageLocator(MessageLocator messageLocator) {
+        this.messageLocator = messageLocator;
+    }
+
+    private BeanGetter rbg;
+    public void setRequestBeanGetter(BeanGetter rbg) {
+        this.rbg = rbg;
+    }
+    public UIJointContainer evolveAttachment(UIInputMany toevolve) {
+        UIJointContainer togo = new UIJointContainer(toevolve.parent, toevolve.ID, COMPONENT_ID);
+        toevolve.parent.remove(toevolve);
+
+        toevolve.ID = "attachments-input";
+        togo.addComponent(toevolve);
+
+        String[] value = toevolve.getValue();
+        // Note that a bound value is NEVER null, an unset value is detected via
+        // this standard call
+        if (UITypes.isPlaceholder(value)) {
+            value = (String[]) rbg.getBean(toevolve.valuebinding.value);
+            // May as well save on later fixups
+            toevolve.setValue(value);
+        }
+
+        UIBranchContainer core = UIBranchContainer.make(togo, CORE_ID);
+        int limit = Math.max(0, value.length);
+        for (int i=0; i < limit; ++i) {
+            UIBranchContainer row = UIBranchContainer.make(core, "attachment-list-row:", Integer.toString(i));
+            String thisvalue = i < value.length ? value[i] : "";
+            try {
+                //TODO - but all contentHosting calls in an external Logic
+                ContentResource cr = contentHostingService.getResource(thisvalue);
+                UILink.make(row, "attachment_image", externalLogic.getContentTypeImagePath(cr));
+                UILink.make(row, "attachment_link", cr.getProperties().getProperty(cr.getProperties().getNamePropDisplayName()),
+                        cr.getUrl());
+                UIOutput.make(row, "attachment_item", thisvalue);
+                UIBasicListMember.makeBasic(row, "attachment_item", toevolve.getFullID(), i);
+                String file_size = externalLogic.getReadableFileSize(cr.getContentLength());
+                UIOutput.make(row, "attachment_size", file_size);
+                //Add remove link
+                UIVerbatim.make(row, "attachment_remove", 
+                        "<a href=\"#\" " +
+                        "onclick=\"" +
+                        "jQuery(this).parent('span').parent('li').remove();updateDisplayNoAttachments();" +
+                        "\">" +
+                        messageLocator.getMessage("assignment2.remove") +
+                "</a>");
+            } catch (Exception e) {
+                //do nothing
+            }
+        }
+
+        if (limit == 0) {
+            //output "demo" row, with styleClass of skip
+            UIBranchContainer row = UIBranchContainer.make(core, "attachment-list-row:", Integer.toString(0));
+            UILink.make(row, "attachment_image", "image.jpg");
+            UILink.make(row, "attachment_link", "demo", "demo.html");
+            //UIOutput.make(row, "attachment_item", "demo");
+            UIBasicListMember.makeBasic(row, "attachment_item", toevolve.getFullID(), 0);
+            UIOutput.make(row, "attachment_size", "demo");
+            //Add remove link
+            UIVerbatim.make(row, "attachment_remove", 
+                    "<a href=\"#\" " +
+                    "onclick=\"" +
+                    "jQuery(this).parent('span').parent('li').remove();updateDisplayNoAttachments();" +
+                    "\">" +
+                    messageLocator.getMessage("assignment2.remove") +
+            "</a>");
+            row.decorators = new DecoratorList(new UIStyleDecorator("skip"));
+        }
+
+        return togo;
+    }
+
+
 }
