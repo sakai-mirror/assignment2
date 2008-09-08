6b4dfa5e109cb9d0a31cfc2c417192a3614846d7
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-09-08 20:10:04 +0000

    NOJIRA Minor code reformatting
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@52865 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/params/ThickboxHelperViewParams.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/params/ThickboxHelperViewParams.java
@@ -25,28 +25,28 @@ import uk.ac.cam.caret.sakai.rsf.helper.HelperViewParameters;
 
 public class ThickboxHelperViewParams extends HelperViewParameters  {
 
-	public Boolean keepThis;
-	public Boolean TB_iframe;
-	public int height;
-	public int width;
-	
-	public ThickboxHelperViewParams() {}
-	
-	public ThickboxHelperViewParams(String viewId) {
-		super(viewId);
-	}
+    public Boolean keepThis;
+    public Boolean TB_iframe;
+    public int height;
+    public int width;
+
+    public ThickboxHelperViewParams() {}
+
+    public ThickboxHelperViewParams(String viewId) {
+        super(viewId);
+    }
 
     public ThickboxHelperViewParams(String viewId, Boolean KeepThis, Boolean TB_iframe, int height, int width){
-    		super(viewId);
-	        this.keepThis = KeepThis;
-	        this.TB_iframe = TB_iframe;
-	        this.height = height;
-	        this.width = width;
+        super(viewId);
+        this.keepThis = KeepThis;
+        this.TB_iframe = TB_iframe;
+        this.height = height;
+        this.width = width;
     }
-    
+
     public String getParseSpec() {
-		// include a comma delimited list of the public properties in this class
-		return super.getParseSpec() + ",keepThis,TB_iframe,height,width";
-	}
+        // include a comma delimited list of the public properties in this class
+        return super.getParseSpec() + ",keepThis,TB_iframe,height,width";
+    }
 }
 
