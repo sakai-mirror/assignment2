8a48c3ecc1888d2bb8c9633f6aa9a20a2df121d6
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-09-23 19:30:46 +0000

    ASNN-206 Minor reformatting and block comment
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@53366 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/producers/StudentAssignmentListProducer.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/producers/StudentAssignmentListProducer.java
@@ -51,14 +51,22 @@ import uk.org.ponder.rsf.viewstate.ViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParamsReporter;
 import uk.org.ponder.htmlutil.HTMLUtil;
 
+/**
+ * This view is responsible for showing the Student Landing page which will have
+ * the Todo style list of assignments that need to be completed.
+ * 
+ * @author rjlowe
+ * @author sgithens
+ *
+ */
 public class StudentAssignmentListProducer implements ViewComponentProducer, ViewParamsReporter {
-	
-	public static final String VIEW_ID = "student-assignment-list";
-	public String getViewID(){
-		return VIEW_ID;
-	}
-	
-	private PagerRenderer pagerRenderer;
+
+    public static final String VIEW_ID = "student-assignment-list";
+    public String getViewID(){
+        return VIEW_ID;
+    }
+
+    private PagerRenderer pagerRenderer;
     private AssignmentLogic assignmentLogic;
     private AssignmentSubmissionLogic submissionLogic;
     private Locale locale;
@@ -71,141 +79,141 @@ public class StudentAssignmentListProducer implements ViewComponentProducer, Vie
     public static final String DEFAULT_SORT_DIR = AssignmentLogic.SORT_DIR_ASC;
     public static final String DEFAULT_OPPOSITE_SORT_DIR = AssignmentLogic.SORT_DIR_DESC;
     public static final String DEFAULT_SORT_BY = AssignmentLogic.SORT_BY_INDEX;
-    
+
     private String current_sort_by = DEFAULT_SORT_BY;
     private String current_sort_dir = DEFAULT_SORT_DIR;
     private String opposite_sort_dir = DEFAULT_OPPOSITE_SORT_DIR;
-    
+
     //images
     public static final String BULLET_UP_IMG_SRC = "/sakai-assignment2-tool/content/images/bullet_arrow_up.png";
     public static final String BULLET_DOWN_IMG_SRC = "/sakai-assignment2-tool/content/images/bullet_arrow_down.png";
     public static final String ATTACH_IMG_SRC = "/sakai-assignment2-tool/content/images/attach.png";
 
-	
-	public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
-    	
-    	// use a date which is related to the current users locale
-		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT, locale);
-    	
-    	//get parameters
-    	AssignmentListSortViewParams params = (AssignmentListSortViewParams) viewparams;
-    	if (params.sort_by == null) params.sort_by = DEFAULT_SORT_BY;
-    	if (params.sort_dir == null) params.sort_dir = DEFAULT_SORT_DIR;
-    	current_sort_by = params.sort_by;
-    	current_sort_dir = params.sort_dir;
-    	opposite_sort_dir = (AssignmentLogic.SORT_DIR_ASC.equals(current_sort_dir) ? AssignmentLogic.SORT_DIR_DESC : AssignmentLogic.SORT_DIR_ASC);
-    	UIVerbatim.make(tofill, "defaultSortBy", HTMLUtil.emitJavascriptVar("defaultSortBy", DEFAULT_SORT_BY));
 
-    	//check if we need to duplicate an assignment, params.assignmentIdToDuplicate is not null
-    	if (params.assignmentIdToDuplicate != null){
-    		assignment2Bean.createDuplicate(params.assignmentIdToDuplicate);
-    		params.assignmentIdToDuplicate = null;
-    	}
-    	
-    	//get paging data
+    public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
+
+        // use a date which is related to the current users locale
+        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT, locale);
+
+        //get parameters
+        AssignmentListSortViewParams params = (AssignmentListSortViewParams) viewparams;
+        if (params.sort_by == null) params.sort_by = DEFAULT_SORT_BY;
+        if (params.sort_dir == null) params.sort_dir = DEFAULT_SORT_DIR;
+        current_sort_by = params.sort_by;
+        current_sort_dir = params.sort_dir;
+        opposite_sort_dir = (AssignmentLogic.SORT_DIR_ASC.equals(current_sort_dir) ? AssignmentLogic.SORT_DIR_DESC : AssignmentLogic.SORT_DIR_ASC);
+        UIVerbatim.make(tofill, "defaultSortBy", HTMLUtil.emitJavascriptVar("defaultSortBy", DEFAULT_SORT_BY));
+
+        //check if we need to duplicate an assignment, params.assignmentIdToDuplicate is not null
+        if (params.assignmentIdToDuplicate != null){
+            assignment2Bean.createDuplicate(params.assignmentIdToDuplicate);
+            params.assignmentIdToDuplicate = null;
+        }
+
+        //get paging data
         List<Assignment2> entries = assignmentLogic.getViewableAssignments();
-    	
-    	//Breadcrumbs
-    	UIMessage.make(tofill, "last_breadcrumb", "assignment2.student-assignment-list.heading");
-    		
+
+        //Breadcrumbs
+        UIMessage.make(tofill, "last_breadcrumb", "assignment2.student-assignment-list.heading");
+
         UIMessage.make(tofill, "page-title", "assignment2.student-assignment-list.title");
         pagerRenderer.makePager(tofill, "pagerDiv:", VIEW_ID, viewparams, entries.size());
-        
+
         //table headers and sorting links
         sortHeaderRenderer.makeSortingLink(tofill, "tableheader.assignment", viewparams, 
-        		AssignmentLogic.SORT_BY_TITLE, "assignment2.student-assignment-list.tableheader.assignment");
+                AssignmentLogic.SORT_BY_TITLE, "assignment2.student-assignment-list.tableheader.assignment");
         sortHeaderRenderer.makeSortingLink(tofill, "tableheader.for", viewparams, 
-        		LocalAssignmentLogic.SORT_BY_FOR, "assignment2.student-assignment-list.tableheader.for");
+                LocalAssignmentLogic.SORT_BY_FOR, "assignment2.student-assignment-list.tableheader.for");
         sortHeaderRenderer.makeSortingLink(tofill, "tableheader.status", viewparams, 
-        		LocalAssignmentLogic.SORT_BY_STATUS, "assignment2.student-assignment-list.tableheader.status");
+                LocalAssignmentLogic.SORT_BY_STATUS, "assignment2.student-assignment-list.tableheader.status");
         sortHeaderRenderer.makeSortingLink(tofill, "tableheader.open", viewparams, 
-        		AssignmentLogic.SORT_BY_OPEN, "assignment2.student-assignment-list.tableheader.open");
+                AssignmentLogic.SORT_BY_OPEN, "assignment2.student-assignment-list.tableheader.open");
         sortHeaderRenderer.makeSortingLink(tofill, "tableheader.due", viewparams, 
-        		AssignmentLogic.SORT_BY_DUE, "assignment2.student-assignment-list.tableheader.due");
-              
+                AssignmentLogic.SORT_BY_DUE, "assignment2.student-assignment-list.tableheader.due");
+
         if (entries.size() <= 0) {
             UIMessage.make(tofill, "assignment_empty", "assignment2.student-assignment-list.assignment_empty");
             return;
         }
-        
+
         // retrieve groups here for display of group restrictions
         Map<String, String> groupIdToNameMap = externalLogic.getGroupIdToNameMapForSite(externalLogic.getCurrentContextId());
-        
+
         // retrieve the statuses for the assignments
         Map<Assignment2, Integer> assignToStatusMap = submissionLogic.getSubmissionStatusConstantForAssignments(entries, externalLogic.getCurrentUserId());
-        
+
         //Fill out Table
         for (Assignment2 assignment : entries){
-        	UIBranchContainer row = UIBranchContainer.make(tofill, "assignment-row:");
-        	
-        	UILink.make(row, "attachments", ATTACH_IMG_SRC);
-        	UIInternalLink.make(row, "assignment_link", assignment.getTitle(), 
-        			new SimpleAssignmentViewParams(StudentSubmitProducer.VIEW_ID, assignment.getId()));
-        	
-        	// Access
-        	String restrictedToText = messageLocator.getMessage("assignment2.assignment_restrict_to_site");
-        	if (assignment.getAssignmentGroupSet() != null && !assignment.getAssignmentGroupSet().isEmpty()) {
-        		// we need to display a comma-delimited list of groups
-        		restrictedToText = localAssignmentLogic.getListOfGroupRestrictionsAsString(
-        				assignment.getAssignmentGroupSet(), groupIdToNameMap);
-        	}
-        	UIOutput.make(row, "assignment_row_for", restrictedToText);
-        	
-        	Integer status = assignToStatusMap.get(assignment);
-        	String statusText = messageLocator.getMessage("assignment2.submission_status." + status.intValue());
-        	UIOutput.make(row, "assignment_row_status", statusText);
-        	
-        	UIOutput.make(row, "assignment_row_open", df.format(assignment.getOpenDate()));
-        	
-        	if (assignment.getDueDate() != null) {
-        		UIOutput.make(row, "assignment_row_due", df.format(assignment.getDueDate()));
-        	} else {
-        		UIMessage.make(row, "assignment_row_due", "assignment2.student-assignment-list.no_due_date");
-        	}
+            UIBranchContainer row = UIBranchContainer.make(tofill, "assignment-row:");
 
-        	//For JS Sorting
-        	UIOutput.make(row, "open_timestamp", assignment.getOpenDate() != null ? String.valueOf(assignment.getOpenDate().getTime()) : "");
-        	UIOutput.make(row, "due_timestamp", assignment.getDueDate() != null ? String.valueOf(assignment.getDueDate().getTime()) : "");
-        	UIOutput.make(row, "sortIndex", String.valueOf(assignment.getSortIndex()));
+            UILink.make(row, "attachments", ATTACH_IMG_SRC);
+            UIInternalLink.make(row, "assignment_link", assignment.getTitle(), 
+                    new SimpleAssignmentViewParams(StudentSubmitProducer.VIEW_ID, assignment.getId()));
+
+            // Access
+            String restrictedToText = messageLocator.getMessage("assignment2.assignment_restrict_to_site");
+            if (assignment.getAssignmentGroupSet() != null && !assignment.getAssignmentGroupSet().isEmpty()) {
+                // we need to display a comma-delimited list of groups
+                restrictedToText = localAssignmentLogic.getListOfGroupRestrictionsAsString(
+                        assignment.getAssignmentGroupSet(), groupIdToNameMap);
+            }
+            UIOutput.make(row, "assignment_row_for", restrictedToText);
+
+            Integer status = assignToStatusMap.get(assignment);
+            String statusText = messageLocator.getMessage("assignment2.submission_status." + status.intValue());
+            UIOutput.make(row, "assignment_row_status", statusText);
+
+            UIOutput.make(row, "assignment_row_open", df.format(assignment.getOpenDate()));
+
+            if (assignment.getDueDate() != null) {
+                UIOutput.make(row, "assignment_row_due", df.format(assignment.getDueDate()));
+            } else {
+                UIMessage.make(row, "assignment_row_due", "assignment2.student-assignment-list.no_due_date");
+            }
+
+            //For JS Sorting
+            UIOutput.make(row, "open_timestamp", assignment.getOpenDate() != null ? String.valueOf(assignment.getOpenDate().getTime()) : "");
+            UIOutput.make(row, "due_timestamp", assignment.getDueDate() != null ? String.valueOf(assignment.getDueDate().getTime()) : "");
+            UIOutput.make(row, "sortIndex", String.valueOf(assignment.getSortIndex()));
         }
-	}
-	
-	public ViewParameters getViewParameters() {
-    	return new AssignmentListSortViewParams();
     }
-	
-	public void setPagerRenderer(PagerRenderer pagerRenderer) {
-		this.pagerRenderer = pagerRenderer;
-	}
-	
+
+    public ViewParameters getViewParameters() {
+        return new AssignmentListSortViewParams();
+    }
+
+    public void setPagerRenderer(PagerRenderer pagerRenderer) {
+        this.pagerRenderer = pagerRenderer;
+    }
+
     public void setAssignmentLogic (AssignmentLogic assignmentLogic) {
-    	this.assignmentLogic = assignmentLogic;
+        this.assignmentLogic = assignmentLogic;
     }
-    
+
     public void setAssignmentSubmissionLogic (AssignmentSubmissionLogic submissionLogic) {
-    	this.submissionLogic = submissionLogic;
+        this.submissionLogic = submissionLogic;
     }
-   
+
     public void setLocale(Locale locale) {
-    	this.locale = locale;
+        this.locale = locale;
     }
-    
+
     public void setAssignment2Bean(Assignment2Bean assignment2Bean) {
-    	this.assignment2Bean = assignment2Bean;
+        this.assignment2Bean = assignment2Bean;
     }
-    
+
     public void setSortHeaderRenderer(SortHeaderRenderer sortHeaderRenderer) {
-    	this.sortHeaderRenderer = sortHeaderRenderer;
+        this.sortHeaderRenderer = sortHeaderRenderer;
     }
-    
-	public void setLocalAssignmentLogic(LocalAssignmentLogic localAssignmentLogic) {
-		this.localAssignmentLogic = localAssignmentLogic;
-	}
-	
-	public void setExternalLogic(ExternalLogic externalLogic) {
-		this.externalLogic = externalLogic;
-	}
-    
+
+    public void setLocalAssignmentLogic(LocalAssignmentLogic localAssignmentLogic) {
+        this.localAssignmentLogic = localAssignmentLogic;
+    }
+
+    public void setExternalLogic(ExternalLogic externalLogic) {
+        this.externalLogic = externalLogic;
+    }
+
     public void setMessageLocator(MessageLocator messageLocator) {
         this.messageLocator = messageLocator;
     }
