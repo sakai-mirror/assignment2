6af2e67bbbb83e23da1f9813b58be31b5adb96a8
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-09-24 17:44:51 +0000

    ASNN-214 Added script and instructions to adjust selenium tests for each specific Sakai installation.
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@53395 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ selenium-test/asnn2_selenium_setup.py
@@ -0,0 +1,61 @@
+#/usr/bin/env python
+"""
+This script is to update the installation specific urls and data
+inside Sakai so that Selenium scripts can be reused.
+
+Replace the new_assn2_page and new_assn2_mainframe variables for
+your local Sakai instance on the Assignments2 page so they look
+like the current_* variables.
+
+Usage:
+
+./asnn2_selenium_setup.py 
+   Update the tests for your local machine.
+
+./asnn2_selenium_setup.py forcheckin
+   Revert the values back to their original values so you can
+   check in new tests.
+
+"""
+import sys
+import re
+import os
+
+current_asnn2_page='/portal/site/usedtools/page/a1d77745-8fbd-49cb-8049-f82df3b482c7'
+current_asnn2_mainframe='Main93edc24cx406bx48a8xb960xd7188a219a0f'
+
+# Replace these two variables for your installation
+new_asnn2_page='/portal/site/usedtools/page/18591cc0-601f-4604-b4bb-44d3d98fc417'
+new_asnn2_mainframe='Main46655112x3373x48a8xbad8x9eb822e1c94c'
+
+def main(args):
+    if new_asnn2_page == '' or new_asnn2_mainframe == '':
+        print '''You need to update the new Sakai information at the
+top of this script. Run again after that.'''
+
+    prepare_checkin = False
+    if len(args) == 1 and args[0] == 'forcheckin':
+        print("Preparing for checkin")
+        sys.exit(0)
+
+    sel_files = [file for file in os.listdir('./') if file.endswith('.html')]
+    for file_name in sel_files:
+        f = open(file_name)
+        content = f.read()
+        f.close()
+    
+        if prepare_checkin == True:
+            content = content.replace(new_asnn2_page, current_asnn2_page)
+            content = content.replace(new_asnn2_mainframe, current_asnn2_mainframe)
+        else:
+            content = content.replace(current_asnn2_page, new_asnn2_page)
+            content = content.replace(current_asnn2_mainframe, new_asnn2_mainframe)
+
+        fixed_f = open(file_name, 'w')
+        fixed_f.write(content)
+        fixed_f.close()
+        print("Fixed up %s" % (file_name))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
