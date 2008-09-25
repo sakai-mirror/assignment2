8ad3e28080287af8639f9b462d50d83bc629b035
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-09-25 14:06:29 +0000

    ASNN-201 Put in another check for preview vs taking. Can't compute whether the student can take it because 1) This is an instructor previewing it and 2) There isn't an assignment id yet because it hasn't been saved
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@53410 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/producers/renderers/StudentViewAssignmentRenderer.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/producers/renderers/StudentViewAssignmentRenderer.java
@@ -263,22 +263,24 @@ public class StudentViewAssignmentRenderer {
         /*
          * Resubmissions Allowed
          */
-        boolean resubmissionsAllowed = submissionLogic.submissionIsOpenForStudentForAssignment(
-                currentUser.getId(), assignment.getId());
-        if (resubmissionsAllowed) {
-            UIMessage.make(joint, "resubmissions-allowed", "assignment2.student-submit.resubmissions_allowed");
-        }
-        else {
-            UIMessage.make(joint, "resubmissions-allowed", "assignment2.student-submit.resubmissions_not_allowed");
-        }
-        
-        /*
-         * Remaining resubmissions allowed
-         */
-        if (resubmissionsAllowed) {
-            UIOutput.make(joint, "remaining-resubmissions-row");
-            //TODO FIXME Michelle is adding in a Logic method to get this info.
-            UIOutput.make(joint, "remaining-resubmissions", "This functionality in progress");
+        if (!preview) {
+            boolean resubmissionsAllowed = submissionLogic.submissionIsOpenForStudentForAssignment(
+                    currentUser.getId(), assignment.getId());
+            if (resubmissionsAllowed) {
+                UIMessage.make(joint, "resubmissions-allowed", "assignment2.student-submit.resubmissions_allowed");
+            }
+            else {
+                UIMessage.make(joint, "resubmissions-allowed", "assignment2.student-submit.resubmissions_not_allowed");
+            }
+            
+            /*
+             * Remaining resubmissions allowed
+             */
+            if (resubmissionsAllowed) {
+                UIOutput.make(joint, "remaining-resubmissions-row");
+                //TODO FIXME Michelle is adding in a Logic method to get this info.
+                UIOutput.make(joint, "remaining-resubmissions", "This functionality in progress");
+            }
         }
         
         /*
