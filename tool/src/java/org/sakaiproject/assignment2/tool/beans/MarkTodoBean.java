d9b5260b315b3f5fad1fbb489d927240037f1d56
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-10-08 17:09:35 +0000

    NOJIRA Block comments
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@53777 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/beans/MarkTodoBean.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/beans/MarkTodoBean.java
@@ -8,20 +8,36 @@ import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.assignment2.logic.AssignmentSubmissionLogic;
 import org.sakaiproject.user.api.User;
 
+/**
+ * This bean is being used primarily from the Student Asnn List view where they
+ * can use a checkbox to mark an assignment as completed, but of course can be
+ * used anywhere else.
+ * 
+ * You just need to set user, assignmentId, and checkTodo properties, then call
+ * markTodo.  
+ * 
+ * This has a dependency on assignmentSubmissionLogic.
+ * 
+ * @author sgithens
+ *
+ */
 public class MarkTodoBean {
     private static Log log = LogFactory.getLog(MarkTodoBean.class);
 
+    // Dependency
     private AssignmentSubmissionLogic assignmentSubmissionLogic;
     public void setAssignmentSubmissionLogic(
             AssignmentSubmissionLogic assignmentSubmissionLogic) {
         this.assignmentSubmissionLogic = assignmentSubmissionLogic;
     }
     
+    // Property
     private User currentUser;
     public void setCurrentUser(User currentUser) {
         this.currentUser = currentUser;
     }
     
+    // Property
     private Long assignmentId;
     public void setAssignmentId(Long assignmentId) {
         this.assignmentId = assignmentId;
@@ -30,6 +46,7 @@ public class MarkTodoBean {
         return assignmentId;
     }
     
+    // Property 
     private boolean checkTodo;
     public void setCheckTodo(boolean checkTodo) {
         this.checkTodo = checkTodo;
@@ -38,6 +55,7 @@ public class MarkTodoBean {
         return checkTodo;
     }
     
+    // Action Method
     public String markTodo() {
         log.warn("Marking Assignment Item: " + assignmentId + " , " + checkTodo);
         
