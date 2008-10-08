aa77b462975ff479bdd9036a76b46116c54e5b9c
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-10-08 16:13:04 +0000

    NOJIRA Block comments and formatting
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@53774 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/params/Assignment2ViewParamsInterceptor.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/params/Assignment2ViewParamsInterceptor.java
@@ -36,71 +36,81 @@ import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParamsInterceptor;
 
+/**
+ * The primary HTTP GET interceptor for Assignments2, that checks URL's for 
+ * permissions and other logic, and then changes where they should be going or
+ * their parameters if necessary.
+ * 
+ * @author rjlowe
+ * @author sgithens
+ *
+ */
 public class Assignment2ViewParamsInterceptor implements ViewParamsInterceptor {
 
-	private LocalPermissionLogic localPermissionLogic;
-	public void setLocalPermissionLogic(LocalPermissionLogic localPermissionLogic){
-		this.localPermissionLogic = localPermissionLogic;
-	}
-	
-	
-	public AnyViewParameters adjustViewParameters(ViewParameters incoming) {
-		if (AuthorizationFailedProducer.VIEWID.equals(incoming.viewID)) {
-			//Always return incoming if we are going to the Authorization Failed Page
-			return incoming;
-		}
-		
-		if (AssignmentDetailProducer.VIEW_ID.equals(incoming.viewID)) {
-			//This is a entitybroker "helper" that is always visible
-			//TODO make sure that this is always visible
-			return incoming;
-		}
-		
-		//Verify View Params for completeness
-		if (incoming instanceof VerifiableViewParams) {
-			if(!((VerifiableViewParams)incoming).verify()){
-				return new AssignmentListSortViewParams(ListProducer.VIEW_ID);
-			}
-		}
-		
-		// depending on the user's perms in the site, will redirect them to either
-		// the "View Submissions" page or Student Summary/Submit page from this generic link
-		if (RedirectToAssignmentProducer.VIEWID.equals(incoming.viewID)) {
-			if (incoming instanceof SimpleAssignmentViewParams) {
-				SimpleAssignmentViewParams params = (SimpleAssignmentViewParams) incoming;
-				if (localPermissionLogic.checkCurrentUserHasViewPermission(new ViewSubmissionsViewParams(ViewSubmissionsProducer.VIEW_ID, params.assignmentId))) {
-					return new ViewSubmissionsViewParams(ViewSubmissionsProducer.VIEW_ID, params.assignmentId);
-				} else if (localPermissionLogic.checkCurrentUserHasViewPermission(new SimpleAssignmentViewParams(StudentSubmitProducer.VIEW_ID, params.assignmentId))) {
-					return new SimpleAssignmentViewParams(StudentSubmitProducer.VIEW_ID, params.assignmentId);
-				}
-			}
-		}
-		
-		//If current user has permission access to requested view
-		if (localPermissionLogic.checkCurrentUserHasViewPermission(incoming)){
-			
-			//now do specific interceptions for student submission pages
-			//Student always has same link, redirect here based on it being open or not
-			if (StudentSubmitProducer.VIEW_ID.equals(incoming.viewID) 
-					|| StudentSubmitSummaryProducer.VIEW_ID.equals(incoming.viewID)) {
-				
-				//Returning newly generated view params causes issues (ASNN-53)
-				String trueDestinationViewID = localPermissionLogic.filterViewIdForStudentSubmission((SimpleAssignmentViewParams)incoming);
-				if (incoming.viewID.equals(trueDestinationViewID)) {
-					return incoming;
-				} else {
-					return new SimpleAssignmentViewParams(trueDestinationViewID, ((SimpleAssignmentViewParams)incoming).assignmentId);
-				}
-			}
-			
-			
-			return incoming;
-		} else if (localPermissionLogic.checkCurrentUserHasViewPermission(new AssignmentListSortViewParams(StudentAssignmentListProducer.VIEW_ID))) {
-			return new AssignmentListSortViewParams(StudentAssignmentListProducer.VIEW_ID);
-			
-		}
-		
-		return new SimpleViewParameters(AuthorizationFailedProducer.VIEWID);
-	}
-	
+    private LocalPermissionLogic localPermissionLogic;
+    public void setLocalPermissionLogic(LocalPermissionLogic localPermissionLogic){
+        this.localPermissionLogic = localPermissionLogic;
+    }
+
+
+    public AnyViewParameters adjustViewParameters(ViewParameters incoming) {
+        if (AuthorizationFailedProducer.VIEWID.equals(incoming.viewID)) {
+            //Always return incoming if we are going to the Authorization Failed Page
+            return incoming;
+        }
+
+        if (AssignmentDetailProducer.VIEW_ID.equals(incoming.viewID)) {
+            //This is a entitybroker "helper" that is always visible
+            //TODO make sure that this is always visible
+            return incoming;
+        }
+
+        //Verify View Params for completeness
+        if (incoming instanceof VerifiableViewParams) {
+            if(!((VerifiableViewParams)incoming).verify()){
+                return new AssignmentListSortViewParams(ListProducer.VIEW_ID);
+            }
+        }
+
+        // depending on the user's perms in the site, will redirect them to either
+        // the "View Submissions" page or Student Summary/Submit page from this generic link
+        if (RedirectToAssignmentProducer.VIEWID.equals(incoming.viewID)) {
+            if (incoming instanceof SimpleAssignmentViewParams) {
+                SimpleAssignmentViewParams params = (SimpleAssignmentViewParams) incoming;
+                if (localPermissionLogic.checkCurrentUserHasViewPermission(new ViewSubmissionsViewParams(ViewSubmissionsProducer.VIEW_ID, params.assignmentId))) {
+                    return new ViewSubmissionsViewParams(ViewSubmissionsProducer.VIEW_ID, params.assignmentId);
+                } else if (localPermissionLogic.checkCurrentUserHasViewPermission(new SimpleAssignmentViewParams(StudentSubmitProducer.VIEW_ID, params.assignmentId))) {
+                    return new SimpleAssignmentViewParams(StudentSubmitProducer.VIEW_ID, params.assignmentId);
+                }
+            }
+        }
+
+        //If current user has permission access to requested view
+        if (localPermissionLogic.checkCurrentUserHasViewPermission(incoming)){
+
+            //now do specific interceptions for student submission pages
+            //Student always has same link, redirect here based on it being open or not
+            if (StudentSubmitProducer.VIEW_ID.equals(incoming.viewID) 
+                    || StudentSubmitSummaryProducer.VIEW_ID.equals(incoming.viewID)) {
+
+                /*
+                 * This is responsible for going to different views depending on
+                 * if the Student can submit still.
+                 */
+                String trueDestinationViewID = localPermissionLogic.filterViewIdForStudentSubmission((SimpleAssignmentViewParams)incoming);
+                if (incoming.viewID.equals(trueDestinationViewID)) {
+                    return incoming;
+                } else {
+                    return new SimpleAssignmentViewParams(trueDestinationViewID, ((SimpleAssignmentViewParams)incoming).assignmentId);
+                }
+            }
+
+            return incoming;
+        } else if (localPermissionLogic.checkCurrentUserHasViewPermission(new AssignmentListSortViewParams(StudentAssignmentListProducer.VIEW_ID))) {
+            return new AssignmentListSortViewParams(StudentAssignmentListProducer.VIEW_ID);
+        }
+
+        return new SimpleViewParameters(AuthorizationFailedProducer.VIEWID);
+    }
+
 }
