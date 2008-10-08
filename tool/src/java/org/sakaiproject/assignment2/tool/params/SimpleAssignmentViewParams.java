a001a15ade5ceaab98e56a54c0859a49c40c77f6
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-10-08 14:42:12 +0000

    NOJIRA Adding block comment and formatting
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@53768 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/params/SimpleAssignmentViewParams.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/params/SimpleAssignmentViewParams.java
@@ -29,35 +29,45 @@ import org.sakaiproject.assignment2.tool.beans.Assignment2Bean;
 import org.sakaiproject.assignment2.tool.producers.StudentSubmitProducer;
 import org.sakaiproject.assignment2.tool.producers.StudentSubmitSummaryProducer;
 
+/**
+ * This view params is for navigating to the apparently student view of an 
+ * assignment, though I'm guessing other views may use it as well.
+ * 
+ * It implements VerifiableViewParams, to allow for auditing/validating itself.
+ * 
+ * @author rjlowe
+ * @author sgithens
+ *
+ */
 public class SimpleAssignmentViewParams extends SimpleViewParameters implements VerifiableViewParams{
 
-	private static final Log LOG = LogFactory.getLog(Assignment2Bean.class);
-	
-	public Long assignmentId;
-	
-	public SimpleAssignmentViewParams() {}
+    private static final Log LOG = LogFactory.getLog(Assignment2Bean.class);
+
+    public Long assignmentId;
+
+    public SimpleAssignmentViewParams() {}
 
     public SimpleAssignmentViewParams(String viewId, Long assignmentId){
-    		super(viewId);
-	        this.assignmentId = assignmentId;
-    }
-    
-    public String getParseSpec() {
-    	return super.getParseSpec() + ",@1:assignmentId";
+        super(viewId);
+        this.assignmentId = assignmentId;
     }
 
-	public Boolean verify()
-	{
-		if (StudentSubmitProducer.VIEW_ID.equals(this.viewID) && this.assignmentId == null){
-			LOG.error("Null assignmentId in viewparamters while attempting to load StudentSubmitProducer");
-			return Boolean.FALSE;
-		}
-		
-		if (StudentSubmitSummaryProducer.VIEW_ID.equals(this.viewID) && this.assignmentId == null) {
-			LOG.error("Null assignmentId in viewparamters while attempting to load StudentSubmitSummaryProducer");
-			return Boolean.FALSE;
-		}
-		
-		return Boolean.TRUE;
-	}
+    public String getParseSpec() {
+        return super.getParseSpec() + ",@1:assignmentId";
+    }
+
+    public Boolean verify()
+    {
+        if (StudentSubmitProducer.VIEW_ID.equals(this.viewID) && this.assignmentId == null){
+            LOG.error("Null assignmentId in viewparamters while attempting to load StudentSubmitProducer");
+            return Boolean.FALSE;
+        }
+
+        if (StudentSubmitSummaryProducer.VIEW_ID.equals(this.viewID) && this.assignmentId == null) {
+            LOG.error("Null assignmentId in viewparamters while attempting to load StudentSubmitSummaryProducer");
+            return Boolean.FALSE;
+        }
+
+        return Boolean.TRUE;
+    }
 }
