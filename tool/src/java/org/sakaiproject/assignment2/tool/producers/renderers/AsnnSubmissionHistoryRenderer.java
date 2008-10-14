305c2d964787c0c2609daff4983639bcf0a5294d
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-10-14 20:11:07 +0000

    ASNN-205 Rendering for multiple submissions.
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@53952 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/producers/renderers/AsnnSubmissionHistoryRenderer.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/producers/renderers/AsnnSubmissionHistoryRenderer.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.Set;
 
 import org.sakaiproject.assignment2.logic.AssignmentSubmissionLogic;
+import org.sakaiproject.assignment2.model.Assignment2;
 import org.sakaiproject.assignment2.model.AssignmentSubmission;
 import org.sakaiproject.assignment2.model.AssignmentSubmissionVersion;
 
+import uk.org.ponder.rsf.components.UIBranchContainer;
 import uk.org.ponder.rsf.components.UIContainer;
 import uk.org.ponder.rsf.components.UIJointContainer;
+import uk.org.ponder.rsf.components.UIOutput;
 import uk.org.ponder.rsf.producers.BasicProducer;
 
 /**
@@ -23,17 +26,20 @@ import uk.org.ponder.rsf.producers.BasicProducer;
  * if any of the aggregated versions have new feedback, an overall notification
  * should be present somewhere at the top of the widget.
  * 
+ * Currently this renderer is marking the submissions as viewed, so only use it
+ * for a student who is actually viewing a submission.
+ * 
  * @author sgithens
  *
  */
 public class AsnnSubmissionHistoryRenderer implements BasicProducer {
-    
+
     // Dependency
     private AsnnSubmissionVersionRenderer asnnSubmissionVersionRenderer;
     public void setAsnnSubmissionVersionRenderer(AsnnSubmissionVersionRenderer asnnSubmissionVersionRenderer) {
         this.asnnSubmissionVersionRenderer = asnnSubmissionVersionRenderer;
     }
-    
+
     // Dependency
     private AssignmentSubmissionLogic submissionLogic;
     public void setSubmissionLogic(AssignmentSubmissionLogic submissionLogic) {
@@ -42,19 +48,31 @@ public class AsnnSubmissionHistoryRenderer implements BasicProducer {
 
     public void fillComponents(UIContainer parent, String clientID, AssignmentSubmission assignmentSubmission) {
         UIJointContainer joint = new UIJointContainer(parent, clientID, "asnn2-submission-history-widget:");       
-        
+
+        Assignment2 assignment = assignmentSubmission.getAssignment();
         List<AssignmentSubmissionVersion> versionHistory = new ArrayList<AssignmentSubmissionVersion>();
         versionHistory = submissionLogic.getVersionHistoryForSubmission(assignmentSubmission);
-        
+
+        if (versionHistory.size() == 1) {
+            AssignmentSubmissionVersion curVersion = versionHistory.get(0);
+            asnnSubmissionVersionRenderer.fillComponents(joint, "single-submission-version:", curVersion);
+            List<Long>versionIds = new ArrayList<Long>();
+            versionIds.add(curVersion.getId());
+            submissionLogic.markFeedbackAsViewed(assignmentSubmission.getId(), versionIds);
+        }
+
         //TODO FIXME We'll have to think about the VERSION 0 logic here
+        UIOutput.make(joint, "multiple-submissions");
+        
         for (AssignmentSubmissionVersion version: versionHistory) {
-            asnnSubmissionVersionRenderer.fillComponents(joint, 
-                    "submission-version:", version);
+            UIBranchContainer versionDiv = UIBranchContainer.make(joint, "submission-version:");
+            UIOutput.make(versionDiv, "header-text", version.getSubmittedDate().toLocaleString());
+            asnnSubmissionVersionRenderer.fillComponents(versionDiv, "submission-entry:", version);
         }
     }
-    
+
     public void fillComponents(UIContainer parent, String clientID) {
-        
+
     }
 
 }
