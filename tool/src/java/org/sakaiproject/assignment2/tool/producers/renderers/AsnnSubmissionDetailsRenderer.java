8855355baab3d6465f276d188b8ec0f8063750e8
wagnermr@iupui.edu <wagnermr@iupui.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-11-26 19:45:16 +0000

    ASNN-394 http://bugs.sakaiproject.org/jira/browse/ASNN-394 SV: Only display resubmission info if assignment requires submission
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@55360 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/producers/renderers/AsnnSubmissionDetailsRenderer.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/producers/renderers/AsnnSubmissionDetailsRenderer.java
@@ -230,42 +230,45 @@ public class AsnnSubmissionDetailsRenderer implements BasicProducer {
 
         /*
          * Resubmissions Allowed
+         * Only display the resubmission information if this assignment requires submission
          */
-        //if (!preview) {
-        boolean resubmissionAllowed = false;
-        Integer subLevelNumSubmissions = assignmentSubmission.getNumSubmissionsAllowed();
-        if (subLevelNumSubmissions != null) {
-            if (subLevelNumSubmissions.equals(AssignmentConstants.UNLIMITED_SUBMISSION) ||
-                    subLevelNumSubmissions.intValue() > 1) {
-                resubmissionAllowed = true;
-            }
-        } else {
-            if (assignment.getNumSubmissionsAllowed() > 1 || 
-                    assignment.getNumSubmissionsAllowed() == AssignmentConstants.UNLIMITED_SUBMISSION) {
-                resubmissionAllowed = true;
-            }
-        }
-
-        if (resubmissionAllowed) {
-            UIMessage.make(joint, "resubmissions-allowed", "assignment2.student-submit.resubmissions_allowed");
-        }
-        else {
-            UIMessage.make(joint, "resubmissions-allowed", "assignment2.student-submit.resubmissions_not_allowed");
-        }
-
-        /*
-         * Remaining resubmissions allowed
-         */
-        if (!previewAsStudent) {
-            UIOutput.make(joint, "remaining-resubmissions-row");
-            int numSubmissionsAllowed = submissionLogic.getNumberOfRemainingSubmissionsForStudent(currentUser.getId(), assignment.getId());
-            String numAllowedDisplay;
-            if (numSubmissionsAllowed == AssignmentConstants.UNLIMITED_SUBMISSION) {
-                numAllowedDisplay = messageLocator.getMessage("assignment2.indefinite_resubmit");
+        if (assignment.isRequiresSubmission()) {
+            UIOutput.make(joint, "resubmission-allowed-row");
+            boolean resubmissionAllowed = false;
+            Integer subLevelNumSubmissions = assignmentSubmission.getNumSubmissionsAllowed();
+            if (subLevelNumSubmissions != null) {
+                if (subLevelNumSubmissions.equals(AssignmentConstants.UNLIMITED_SUBMISSION) ||
+                        subLevelNumSubmissions.intValue() > 1) {
+                    resubmissionAllowed = true;
+                }
             } else {
-                numAllowedDisplay = "" + numSubmissionsAllowed;
+                if (assignment.getNumSubmissionsAllowed() > 1 || 
+                        assignment.getNumSubmissionsAllowed() == AssignmentConstants.UNLIMITED_SUBMISSION) {
+                    resubmissionAllowed = true;
+                }
             }
-            UIOutput.make(joint, "remaining-resubmissions", numAllowedDisplay);
+
+            if (resubmissionAllowed) {
+                UIMessage.make(joint, "resubmissions-allowed", "assignment2.student-submit.resubmissions_allowed");
+            }
+            else {
+                UIMessage.make(joint, "resubmissions-allowed", "assignment2.student-submit.resubmissions_not_allowed");
+            }
+
+            /*
+             * Remaining resubmissions allowed
+             */
+            if (!previewAsStudent && resubmissionAllowed) {
+                UIOutput.make(joint, "remaining-resubmissions-row");
+                int numSubmissionsAllowed = submissionLogic.getNumberOfRemainingSubmissionsForStudent(currentUser.getId(), assignment.getId());
+                String numAllowedDisplay;
+                if (numSubmissionsAllowed == AssignmentConstants.UNLIMITED_SUBMISSION) {
+                    numAllowedDisplay = messageLocator.getMessage("assignment2.indefinite_resubmit");
+                } else {
+                    numAllowedDisplay = "" + numSubmissionsAllowed;
+                }
+                UIOutput.make(joint, "remaining-resubmissions", numAllowedDisplay);
+            }
         }
     }
 
