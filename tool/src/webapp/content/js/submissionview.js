1cc28e5b9086569ab3b71cf0b64e29a1e3eeacf0
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-05-20 20:36:34 +0000

    ASNN-468 Markup in for the new sorted/paged table and hooking up javascript
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@60619 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/submissionview.js
+++ tool/src/webapp/content/js/submissionview.js
@@ -1,7 +1,43 @@
 var asnn2subview = asnn2subview || {};
 
+asnn2subview.selectorMap = [
+  { selector: ".row", id: "row" },
+  { selector: ".student-name", id: "student-name"},
+  { selector: ".submitted-time", id: "submitted-time"},
+  { selector: ".submission-status", id: "submission-status"},
+  { selector: ".grade", id: "grade"},
+  { selector: ".feedback-released", id: "feedback-released"}
+];
+
+asnn2subview.initPager = function(data) {
+
+  var columnDefs = [
+    {
+      key: "student-name",
+      valuebinding: "studentName",
+      sortable: false
+    }
+  ];
+
+  var pager = fluid.pager("#submissions-table-area", {
+    dataModel: data,
+    columnDefs: columnDefs,
+
+    bodyRenderer: {
+      type: "fluid.pager.selfRender",
+      options: {
+        selectors: {
+          root: ".fl-pager-data"
+        },
+        renderOptions: {debugMode: true, cutpoints: asnn2subview.selectorMap}
+      }
+    }
+
+  });
+
+};
+
 asnn2subview.init = function() {
-  alert("Hello Submission View!");
 
   jQuery.ajax({
     type: "GET",
@@ -9,7 +45,9 @@ asnn2subview.init = function() {
     cache: false,
     success: function (payload) {
       var data = JSON.parse(payload);
-      alert(JSON.stringify(data));
+      togo = fluid.transform(data.assignment2submission_collection, asnn2util.dataFromEntity);
+      asnn2subview.initPager(togo);
     }
+    // TODO Handle the failure case
   });
 };
