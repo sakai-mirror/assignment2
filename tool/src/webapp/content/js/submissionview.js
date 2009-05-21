cd07ac367d72bf764bd7647f6cd52c9ec3c99eb1
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-05-21 21:34:05 +0000

    ASNN-468 Prototyping and messing with the submissions table
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@60665 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/submissionview.js
+++ tool/src/webapp/content/js/submissionview.js
@@ -1,12 +1,14 @@
 var asnn2subview = asnn2subview || {};
 
 asnn2subview.selectorMap = [
-  { selector: ".row", id: "row" },
-  { selector: ".student-name", id: "student-name"},
-  { selector: ".submitted-time", id: "submitted-time"},
-  { selector: ".submission-status", id: "submission-status"},
+  { selector: ".row", id: "row:" },
+  { selector: ".sub-table-header", id: "header:" },
+  { selector: ".student-name", id: "studentName"},
+  { selector: ".submitted-time", id: "submittedDateFormat"},
+  { selector: ".submitted-date", id: "submittedDate"},
+  { selector: ".submission-status", id: "submissionStatus"},
   { selector: ".grade", id: "grade"},
-  { selector: ".feedback-released", id: "feedback-released"}
+  { selector: ".feedback-released", id: "feedbackReleased"}
 ];
 
 asnn2subview.initPager = function(data) {
@@ -14,22 +16,49 @@ asnn2subview.initPager = function(data) {
   var columnDefs = [
     {
       key: "student-name",
-      valuebinding: "studentName",
-      sortable: false
+      valuebinding: "*.studentName",
+      sortable: true
+    },
+    {
+      key: "submitted-time",
+      valuebinding: "*.submittedDateFormat",
+      sortable: true
+    },
+    {
+      key: "submission-status",
+      valuebinding: "*.submissionStatus",
+      sortable: true
+    },
+    {
+      key: "feedback-released",
+      valuebinding: "*.feedbackReleased",
+      sortable: true
     }
   ];
+
+  var pagerBarOptions = {
+          type: "fluid.pager.pagerBar",
+          options: {
+            pageList: {
+               type: "fluid.pager.renderedPageList"
+             //  options: {
+             //    pageStrategy: fluid.pager.gappedPageStrategy(3, 1)
+             //    }
+               }
+            }
+      };
 
   var pager = fluid.pager("#submissions-table-area", {
     dataModel: data,
     columnDefs: columnDefs,
-
+    pagerBar: pagerBarOptions,
     bodyRenderer: {
       type: "fluid.pager.selfRender",
       options: {
         selectors: {
           root: ".fl-pager-data"
         },
-        renderOptions: {debugMode: true, cutpoints: asnn2subview.selectorMap}
+        renderOptions: {debugMode: false, cutpoints: asnn2subview.selectorMap}
       }
     }
 
