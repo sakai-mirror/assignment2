469567d51dcf5f703d3ed8d97b36b9959e166182
wagnermr@iupui.edu <wagnermr@iupui.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-06-01 12:46:43 +0000

    svn merge -r60618:60665 https://source.sakaiproject.org/contrib/assignment2/trunk --- Merging r60619 through r60665 into '.': A    tool/src/webapp/content/js/FluidPager.js U    tool/src/webapp/content/js/submissionview.js A    tool/src/webapp/content/js/InfusionAll_r7199.js U    tool/src/webapp/content/templates/Layout.html U    tool/src/webapp/content/templates/viewSubmissions.html
    r60619 | swgithen@mtu.edu | 2009-05-20 16:36:34 -0400 (Wed, 20 May 2009) | 1 line
    
    ASNN-468 Markup in for the new sorted/paged table and hooking up javascript
    ------------------------------------------------------------------------
    r60665 | swgithen@mtu.edu | 2009-05-21 17:34:05 -0400 (Thu, 21 May 2009) | 1 line
    
    ASNN-468 Prototyping and messing with the submissions table
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/sakai_2-6-x@60888 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/submissionview.js
+++ tool/src/webapp/content/js/submissionview.js
@@ -1,7 +1,72 @@
 var asnn2subview = asnn2subview || {};
 
+asnn2subview.selectorMap = [
+  { selector: ".row", id: "row:" },
+  { selector: ".sub-table-header", id: "header:" },
+  { selector: ".student-name", id: "studentName"},
+  { selector: ".submitted-time", id: "submittedDateFormat"},
+  { selector: ".submitted-date", id: "submittedDate"},
+  { selector: ".submission-status", id: "submissionStatus"},
+  { selector: ".grade", id: "grade"},
+  { selector: ".feedback-released", id: "feedbackReleased"}
+];
+
+asnn2subview.initPager = function(data) {
+
+  var columnDefs = [
+    {
+      key: "student-name",
+      valuebinding: "*.studentName",
+      sortable: true
+    },
+    {
+      key: "submitted-time",
+      valuebinding: "*.submittedDateFormat",
+      sortable: true
+    },
+    {
+      key: "submission-status",
+      valuebinding: "*.submissionStatus",
+      sortable: true
+    },
+    {
+      key: "feedback-released",
+      valuebinding: "*.feedbackReleased",
+      sortable: true
+    }
+  ];
+
+  var pagerBarOptions = {
+          type: "fluid.pager.pagerBar",
+          options: {
+            pageList: {
+               type: "fluid.pager.renderedPageList"
+             //  options: {
+             //    pageStrategy: fluid.pager.gappedPageStrategy(3, 1)
+             //    }
+               }
+            }
+      };
+
+  var pager = fluid.pager("#submissions-table-area", {
+    dataModel: data,
+    columnDefs: columnDefs,
+    pagerBar: pagerBarOptions,
+    bodyRenderer: {
+      type: "fluid.pager.selfRender",
+      options: {
+        selectors: {
+          root: ".fl-pager-data"
+        },
+        renderOptions: {debugMode: false, cutpoints: asnn2subview.selectorMap}
+      }
+    }
+
+  });
+
+};
+
 asnn2subview.init = function() {
-  alert("Hello Submission View!");
 
   jQuery.ajax({
     type: "GET",
@@ -9,7 +74,9 @@ asnn2subview.init = function() {
     cache: false,
     success: function (payload) {
       var data = JSON.parse(payload);
-      alert(JSON.stringify(data));
+      togo = fluid.transform(data.assignment2submission_collection, asnn2util.dataFromEntity);
+      asnn2subview.initPager(togo);
     }
+    // TODO Handle the failure case
   });
 };
