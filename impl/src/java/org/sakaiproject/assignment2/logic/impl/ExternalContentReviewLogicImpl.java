5ff83c9a61bc79ebf1da287a400b79154c5dc1ac
wagnermr@iupui.edu <wagnermr@iupui.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-06-26 12:33:26 +0000

    ASNN-516 Initialize the ExternalContentReviewLogic
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@61463 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/src/java/org/sakaiproject/assignment2/logic/impl/ExternalContentReviewLogicImpl.java
+++ impl/src/java/org/sakaiproject/assignment2/logic/impl/ExternalContentReviewLogicImpl.java
@@ -1,9 +1,17 @@
 package org.sakaiproject.assignment2.logic.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.assignment2.logic.ExternalContentReviewLogic;
 
 public class ExternalContentReviewLogicImpl implements ExternalContentReviewLogic {
 
+    private static Log log = LogFactory.getLog(ExternalContentReviewLogicImpl.class);
+    
+    public void init(){
+        if(log.isDebugEnabled()) log.debug("init");
+    }
+    
     public boolean isContentReviewAvailable() {
         return false;
     }
