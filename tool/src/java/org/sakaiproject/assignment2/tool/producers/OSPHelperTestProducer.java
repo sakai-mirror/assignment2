004e777b4dde61d742ccdcc7c69bbb95d96f03fd
swgithen@mtu.edu <swgithen@mtu.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-02-09 16:41:08 +0000

    ASNN-609 ONC-743 Successfully reproducing the problem now
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/ASNN-609@66077 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/assignment2/tool/producers/OSPHelperTestProducer.java
+++ tool/src/java/org/sakaiproject/assignment2/tool/producers/OSPHelperTestProducer.java
@@ -21,18 +21,49 @@ import uk.org.ponder.rsf.view.ViewComponentProducer;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
 
 public class OSPHelperTestProducer implements ViewComponentProducer {
-	public static final String VIEW_ID = "osptest";
+    public static final String VIEW_ID = "osptest";
+    public static final String MATRIX_PROV = 
+        "org.theospi.portfolio.tagging.api.MatrixTaggingProvider";
 
-	@Override
-	public void fillComponents(UIContainer tofill, ViewParameters viewparams,
-			ComponentChecker checker) {
-		UIInternalLink addAttachLink = UIInternalLink.make(tofill, "add_attachments", UIMessage.make("assignment2.assignment_add.add_attachments"),
-				new FilePickerHelperViewParams(AddAttachmentHelperProducer.VIEWID, Boolean.TRUE, 
-						Boolean.TRUE, 500, 700, "OTPKey"));
+    public void fillComponents(UIContainer tofill, ViewParameters viewparams,
+            ComponentChecker checker) {
+        
+        UIInternalLink addAttachLink = UIInternalLink.make(tofill, "add_attachments", UIMessage.make("assignment2.assignment_add.add_attachments"),
+                new FilePickerHelperViewParams(AddAttachmentHelperProducer.VIEWID, Boolean.TRUE, 
+                        Boolean.TRUE, 500, 700, "OTPKey"));
 
-	}
-	private void renderMatrixTagging() {
-		/*** Removing support for Assignments2 and matrix linking for now */
+        renderCopiedA1Test(tofill);
+    }
+
+
+    /**
+     * This is a test, with parameters copied straight from an A1 launch of the
+     * helper stopped with the debugger.
+     */
+    private void renderCopiedA1Test(UIContainer tofill) {
+        // This is copied from my local instance, need to change if prototyping
+        // on a difference box.
+        String activityRef = 
+            "/assignment/a/cleantools/187af2c9-aae3-42d0-8a83-c223b03e1613";
+
+        TaggingManager taggingManager = (TaggingManager) ComponentManager
+        .get("org.sakaiproject.taggable.api.TaggingManager");
+        TaggingProvider provider = taggingManager.findProviderById(MATRIX_PROV);
+
+        TaggingHelperInfo helperInfo = provider
+            .getActivityHelperInfo(activityRef);
+
+        UIInternalLink.make(tofill, "link_matrix", helperInfo.getName(),
+                new TaggableHelperViewParams(TaggableHelperProducer.VIEWID, 
+                        helperInfo.getHelperId(), 
+                        helperInfo.getParameterMap().keySet().toArray(new String[0]), 
+                        helperInfo.getParameterMap().values().toArray(new String[0])));
+    }
+
+
+    private void renderMatrixTagging() {
+        /*** Removing support for Assignments2 and matrix linking for now */
+        /*
 	        TaggingManager taggingManager = (TaggingManager) ComponentManager.get("org.sakaiproject.taggable.api.TaggingManager");
 	        if (taggingManager.isTaggable() && assignment != null){
 	                //TODO: optimize?
@@ -66,10 +97,10 @@ public class OSPHelperTestProducer implements ViewComponentProducer {
 	                        }
 	                }
 	        }
+         */
+    }
 
-	}
-	
-	private List<DecoratedTaggingProvider> initDecoratedProviders() {
+    private List<DecoratedTaggingProvider> initDecoratedProviders() {
         TaggingManager taggingManager = (TaggingManager) ComponentManager
         .get("org.sakaiproject.taggable.api.TaggingManager");
         List<DecoratedTaggingProvider> providers = new ArrayList<DecoratedTaggingProvider>();
@@ -81,9 +112,8 @@ public class OSPHelperTestProducer implements ViewComponentProducer {
     }
 
 
-	@Override
-	public String getViewID() {
-		return VIEW_ID;
-	}
+    public String getViewID() {
+        return VIEW_ID;
+    }
 
 }
