337f1b5a7bf817e7c761c05930596b8e24b78699
wagnermr@iupui.edu <wagnermr@iupui.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-03-25 20:26:08 +0000

    ASNN-631 update to r66863
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/ASNN-631@66864 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- sql/mysql/ASNN-617.sql
+++ sql/mysql/ASNN-617.sql
@@ -8,7 +8,7 @@ INSERT INTO SAKAI_REALM_FUNCTION VALUES (DEFAULT, 'asnn2.assignment.delete');
 INSERT INTO SAKAI_REALM_FUNCTION VALUES (DEFAULT, 'asnn2.submissions.manage');
 INSERT INTO SAKAI_REALM_FUNCTION VALUES (DEFAULT, 'asnn2.all.groups');
 
---Project Sites
+-- Project Sites
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'maintain'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'asnn2.assignment.read'));
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'maintain'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'asnn2.assignment.new'));
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'maintain'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'asnn2.assignment.edit'));
@@ -30,7 +30,7 @@ INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where RE
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!group.template'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'access'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'asnn2.submit'));
 
 
---Course Sites
+-- Course Sites
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template.course'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'Instructor'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'asnn2.assignment.read'));
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template.course'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'Instructor'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'asnn2.assignment.new'));
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template.course'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'Instructor'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'asnn2.assignment.edit'));
