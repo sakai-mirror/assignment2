917c620b035ed2463d56fb014a28441988d66888
dsobiera@indiana.edu <dsobiera@indiana.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-04-07 18:15:17 +0000

    ASNN-735 script to patch for sakai 2.7.x and 2.8.x
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/trunk@73655 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ prepare_for_sakai_env.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+getSvn()
+{
+  echo "getting svn " $1
+  svn checkout $1
+}
+
+compileInstall()
+{
+  echo "compiling installing"
+  mvn clean install
+}
+
+compileDeploy()
+{
+  echo "compiling deploying"
+  mvn clean install sakai:deploy
+}
+
+applyPatch()
+{
+  echo "applying patch " $1
+}
+
+
+
+while [ "$version" != "1" -a "$version" != "2" -a "$version" != "3" -a "$version" != "4" ] 
+do
+  clear
+  echo
+  echo "This script will download (into a directory in assignment2 named temp), compile and install into your"
+  echo "local maven repository some dependencies.  It will patch the dependencies' pom.xml files as well as patch"
+  echo "assignment2's pom.xml files to work w/ your branch of sakai.  It will ONLY patch files within the assignment2"
+  echo "tree (it does NOT touch anything outside of it).  This assignment2 directory MUST be sitting inside"
+  echo "your Sakai tree before you try to run this script."
+  echo
+  echo "(1) - Sakai 2.7.x"
+  echo "(2) - Sakai 2.8.x"
+  echo "(3) - Sakai 2.9.x/trunk"
+  echo 
+  echo "(4) - Exit"
+  echo
+  echo "Please choose sakai verson to patch the tool for: \c "
+  read version
+done
+
+case "$version" in
+        1)
+                echo "Preparing for a Sakai 2.7.x environment...."
+                getSvn "https://source.sakaiproject.org/svn/content-review/trunk temp/content-review-2.9.x"
+                cd temp/content-review-2.9.x
+                compileInstall
+                cd ../..
+                getSvn "https://source.sakaiproject.org/svn/taggable/trunk temp/taggable-2.9.x"
+                cd temp/taggable-2.9.x
+                patch -p1 -i ../../patches/taggable_sakai_2_7_x.patch
+                compileInstall
+                cd ../..
+                patch -p1 -i patches/assignment2_sakai_2_7_x.patch
+                echo
+                echo "Succesfully prepared assignment2 for a sakai 2.7.x environment."
+                echo "You should be able to build and deploy assignment2 now."
+                echo
+
+                break
+                ;;
+        2)
+                echo "Preparing for a Sakai 2.8.x environment...."
+                getSvn "https://source.sakaiproject.org/svn/content-review/trunk temp/content-review-2.9.x"
+                cd temp/content-review-2.9.x
+                compileInstall
+                cd ../..
+                getSvn "https://source.sakaiproject.org/svn/taggable/trunk temp/taggable-2.9.x"
+                cd temp/taggable-2.9.x
+                patch -p1 -i ../../patches/taggable_sakai_2_8_x.patch
+                compileInstall
+                cd ../..
+                patch -p1 -i patches/assignment2_sakai_2_8_x.patch
+                echo
+                echo "Succesfully prepared assignment2 for a sakai 2.8.x environment."
+                echo "You should be able to build and deploy assignment2 now."
+                echo
+
+                break
+                ;;
+        3)
+                echo "Sakai 2.9.x/trunk"
+                echo 
+                echo "Nothing needs to be done for this version"
+                echo
+
+                break
+                ;;
+        4)
+                echo "Exit"
+                echo 
+                
+                break
+                ;;
+        *)
+                echo "Invalid entry"
+                ;;
+esac
+
+
+
+
