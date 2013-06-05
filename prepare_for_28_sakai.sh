0124520b36d683c3858fe69e2f27acd3bdedac0c
dsobiera@indiana.edu <dsobiera@indiana.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2013-06-05 17:27:13 +0000

    ASNN-821 - de-trunkify branch
    git-svn-id: https://source.sakaiproject.org/contrib/assignment2/branches/sakai-2.9.x@83884 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- prepare_for_28_sakai.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Stubbing out taggable for ..."
-patch -p0 < patches/remove-taggable-functionality-stub-false.patch
-echo "Removing edit points functionality ..."
-patch -p0 < patches/editPoints.patch
-echo "Reverting Spring 2->3 changes..."
-patch -R -p0 < patches/springUpdate.patch    
-
